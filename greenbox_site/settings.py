557190821c5ee04ad0e9a7391b317fa7eaa49dfd
lyjiebox <lyjiebox@qq.com>
2025-03-27 00:08:25 +0800

    更新settings.py 配置，为了线上站点能登录后台

--- greenbox_site/settings.py
+++ greenbox_site/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t5v+hvhywag!y=f4@-l&#$i15+*qwdl8m*s1zgp3l8*3x=!-5+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['boxenno.vip', 'www.boxenno.vip']
 
+CSRF_TRUSTED_ORIGINS = ['https://boxenno.vip', 'https://www.boxenno.vip']
 
 # Application definition
 
@@ -128,3 +129,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+CSRF_COOKIE_DOMAIN = '.boxenno.vip'
+CSRF_COOKIE_HTTPONLY = False
